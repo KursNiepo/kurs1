654d3e1869e41fccd97ef726c1335b04eac323e5
pszczesz <pszczesz@gmail.com>
2017-05-20 09:14:08 +0200

    first

---
+++ Podstawy1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podstawy1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.Write("Podaj a: ");
+            string vala = Console.ReadLine();
+            Console.Write("Podaj b: ");
+            string valb = Console.ReadLine();
+            int aa = Convert.ToInt32(vala);
+            int bb = Convert.ToInt32(valb);
+            
+            Console.WriteLine(aa+" + "+bb+" = "+(aa+bb));
+            Console.ReadKey();
+        }
+    }
+}
